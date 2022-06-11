0f3c86539518a64ef589551ccb5875d8fed6edfb
Amin Rezaei <AminRezaei0x443@gmail.com>
2022-06-11 19:53:08 +0430

    feature: implement batching and fast jit optimizations

--- scann/__init__.py
+++ scann/__init__.py
@@ -1,3 +1,4 @@
 from scann.util import anisotropic_weights, loss_function, loss_function_batch
-from scann.vq import vector_quantize
-from scann.pq import product_quantize
+from scann.quantizer import Quantizer
+from scann.vq import VectorQuantizer
+from scann.pq import ProductQuantizer
