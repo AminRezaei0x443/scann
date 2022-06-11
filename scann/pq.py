d43a1c636d33f7920a86cf3a2c3fe747ae82c789
Amin Rezaei <AminRezaei0x443@gmail.com>
2022-06-11 21:33:22 +0430

    update: heavy optimizations

--- scann/pq.py
+++ scann/pq.py
@@ -2,14 +2,27 @@
 import jax.numpy as jnp
 from jax.experimental.sparse import BCOO
 import jax.scipy.sparse.linalg as jla
-from jax import lax, vmap
+from jax import lax, vmap, jit
+from functools import partial
+import jax.random as jrnd
 from scann import VectorQuantizer, anisotropic_weights, loss_function, Quantizer
 
 
 class ProductQuantizer(Quantizer):
-    def __init__(self, m, k, T):
+    def __init__(self, m, k, T, max_assign_steps=3):
         self.m, self.k, self.T = (m, k, T)
+        self.max_assign_steps = max_assign_steps
+
+        self.batch_assign = vmap(self.assign, (None, 0), (0, 0))
+        self.batch_build = vmap(self.calculate_B_i, (0,), 0)
+        self.batch_calc = vmap(self.calculate_BTx, (0, 0), (0, 0))
     
+    def __hash__(self) -> int:
+        return hash((
+            self.m, self.k, self.T, self.max_assign_steps
+        ))
+
+    @partial(jit, static_argnums=0)
     def calculate_B_i(self, I_n):
         indices = []
         values = jnp.array([], dtype=jnp.bool_)
@@ -25,8 +38,8 @@ class ProductQuantizer(Quantizer):
         indices = jnp.array(indices, dtype=jnp.int32)
         B_i = BCOO((values, indices), shape=(self.f, self.f * self.k))
         return B_i
-    
-    def assign(self, c, x, max_assign_steps = 3):
+
+    def assign(self, c, x):
         a_iter = 0
         a_gh = True
         # v-quantized
@@ -56,9 +69,10 @@ class ProductQuantizer(Quantizer):
                 pass
             a_iter += 1
             # add check of change
-            a_gh = (a_iter < max_assign_steps)
+            a_gh = (a_iter < self.max_assign_steps)
         return last_l, assignment_row
 
+    @partial(jit, static_argnums=0)
     def calculate_BTx(self, B_i, x):
         fx = x.reshape((1, x.shape[0]))
         _, h_o, h_p = self.weights
@@ -67,20 +81,31 @@ class ProductQuantizer(Quantizer):
         r = lm @ B_i
         return r, h_p * (B_i.T @ fx.T)
 
-    def fit(self, X, tol=1e-2, max_iter=100):
+    def fit(self, X, tol=1e-2, max_iter=100, vq_impl=True):
         # m -> number of codebooks
         # k -> number of centroids
         data = X
         self.n, self.f = data.shape
-        self.weights = eta, h_o, h_p = anisotropic_weights(self.T, self.f)
+        self.weights = anisotropic_weights(self.T, self.f)
         # normalize 
         nX = data / jnp.linalg.norm(data, axis=1, keepdims=True)
         # init 
         # C: k x f
-        vq = VectorQuantizer(self.k, self.T)
-        vq.fit(data, tol, max_iter)
-        C = vq.C
-        self.c = C.reshape(-1)
+        if vq_impl:
+            vq = VectorQuantizer(self.k, self.T)
+            vq.fit(data, tol, max_iter)
+            C = vq.C
+            self.c = C.reshape(-1)
+        else:
+            if self.m == 1:
+                key = jrnd.PRNGKey(0)
+                key, sub_key = jrnd.split(key)
+                C = jrnd.choice(sub_key, nX, shape=(self.k,), replace=False)
+                self.c = C.reshape(-1)
+            else:
+                pq = ProductQuantizer(1, self.k, self.T, max_assign_steps=1)
+                pq.fit(data, tol, max_iter)
+                self.c = pq.c
         # int16 ? better'd be determine by k, but need to be efficient too
         self.I = jnp.zeros((self.n, self.m), dtype=jnp.int16)
         # repeat
@@ -89,12 +114,9 @@ class ProductQuantizer(Quantizer):
         loss = 0
 
         while go_ahead:
-            # assingment step
-            n_loss = 0
-            
+            # assingment step            
             print("assigning ...")
-            batch_assign = vmap(self.assign, (None, 0), (0, 0))
-            a_l, self.I = batch_assign(self.c, nX)
+            a_l, self.I = self.batch_assign(self.c, nX)
             n_loss = jnp.sum(a_l)
             print("loss after assigns ->", n_loss)
             
@@ -104,12 +126,10 @@ class ProductQuantizer(Quantizer):
             # i_x -> pos
             # i_k * i_m * f//m , + f//m
             print("creating B_i ...")
-            batch_build = vmap(self.calculate_B_i, (0,), 0)
-            B = batch_build(self.I)
+            B = self.batch_build(self.I)
 
             print("calculating BTB, BTx ...")
-            batch_calc = vmap(self.calculate_BTx, (0, 0), (0, 0))
-            BTBb, BTxb = batch_calc(B, nX)
+            BTBb, BTxb = self.batch_calc(B, nX)
             BTB = BTBb.sum(axis=0)
             BTx = BTxb.sum(axis=0)
             
@@ -121,7 +141,6 @@ class ProductQuantizer(Quantizer):
             iters += 1
             go_ahead = (abs(n_loss - loss) > tol) and (iters < max_iter)
             loss = n_loss
-        self.c = c
 
     def quantize(self, x):
         return super().quantize(x)
--- scann/vq.py
+++ scann/vq.py
@@ -1,8 +1,9 @@
+from functools import partial
 import jax.scipy.sparse.linalg as jla
 import jax
 import jax.numpy as jnp
 import jax.random as jrnd
-from scann import loss_function, anisotropic_weights, loss_function_batch, Quantizer
+from scann import loss_function, anisotropic_weights, Quantizer
 import jax.experimental.host_callback as hcb
 
 
@@ -10,11 +11,15 @@ class VectorQuantizer(Quantizer):
     def __init__(self, k, T):
         self.k = k
         self.T = T
-    
-    def optimize_center(self, X_j):
+        # compile functions
+        self.batch_optimize_center = jax.jit(jax.vmap(self.optimize_center, (0, 0), 0))
+        self.batch_find_center = jax.jit(jax.vmap(self.find_center, (0, None), (0, 0)))
+        self.batch_loss = jax.jit(jax.vmap(loss_function, (None, 0, None), 0))
+
+    def optimize_center(self, X_j, nz):
         _, h_o, h_p = self.weights
         _, f = X_j.shape
-        A = (jnp.eye(f) * len(X_j) * h_o)
+        A = (jnp.eye(f) * nz * h_o)
         A += (h_p - h_o) * (X_j.T @ X_j)
 
         b = h_p * jnp.sum(X_j, axis=0)
@@ -22,17 +27,17 @@ class VectorQuantizer(Quantizer):
         nc, _ = jla.cg(A, b)
         return nc 
 
-    def find_center(self, x, C, normalize=True):
-        losses = jax.lax.map(lambda c: loss_function(x, c, self.weights, normalize=normalize), C)
+    def find_center(self, x, C, normalize=False):
+        losses = self.batch_loss(x, C, self.weights)
         # in case of need fore debugging in jaxpr functions (jax.map, jax.jit, ...)
         # hcb.id_print(losses)
         i = jnp.argmin(losses)
-        return i
+        return i, losses[i]
 
     def fit(self, X, tol=1e-2, max_iter=100):
         data = X
         self.n, self.f = data.shape
-        self.weights = eta, h_o, h_p = anisotropic_weights(self.T, self.f)
+        self.weights = anisotropic_weights(self.T, self.f)
         # normalize 
         nX = data / jnp.linalg.norm(data, axis=1, keepdims=True)
         # init 
@@ -46,16 +51,22 @@ class VectorQuantizer(Quantizer):
         loss = 0
         while go_ahead:
             # partition assignment
-            I_c = jax.lax.map(lambda x: self.find_center(x, C, normalize=False), nX)
+            I_c, losses = self.batch_find_center(nX, C)
             # loss
-            X_q = jax.lax.map(lambda i: C[i, :], I_c)
-            n_loss = loss_function_batch(nX, X_q, (eta, h_o, h_p), normalize=False)
+            n_loss = losses.sum()
             print("loss ->", n_loss)
             # codebook optimization
-            
+            masks = []
+            nzs = []
             for j in range(self.k):
-                X_j = nX[I_c == j]
-                C = C.at[j].set(self.optimize_center(X_j))
+                mask = jnp.repeat((I_c == j).reshape(self.n, 1), self.f, axis=1)
+                X_j = nX * mask
+                masks.append(X_j)
+                nzs.append((I_c == j).sum())
+
+            masks = jnp.stack(masks)
+            nzs = jnp.stack(nzs)
+            C = self.batch_optimize_center(masks, nzs)
 
             iters += 1
             go_ahead = (abs(n_loss - loss) > tol) and (iters < max_iter)
